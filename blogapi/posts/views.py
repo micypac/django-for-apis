e79531869e728ee605bf4674711a35167bf44800
Michael Pacleb <michael.pacleb@gmail.com>
2023-06-21 17:41:08 -0400

    created custom permissions that only authenticated users can view post list and post detail pages and the author to update/delete post detail page.

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -2,14 +2,17 @@ from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
