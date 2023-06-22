eb86047a2f21839bff7490b81375b29af1822ef0
Michael Pacleb <michael.pacleb@gmail.com>
2023-06-21 21:02:07 -0400

    created two new endpoints to display list of users and user detail with their corresponding views, serializers, and url path.

--- blogapi/posts/serializers.py
+++ blogapi/posts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Post
+from django.contrib.auth import get_user_model
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -12,4 +13,13 @@ class PostSerializer(serializers.ModelSerializer):
             "title",
             "body",
             "created_at",
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "username",
         )
--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, permissions
 
+from django.contrib.auth import get_user_model
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
@@ -16,3 +17,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
