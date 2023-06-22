8c1f88aff013bc97ae1dbe83b36e60b065e125f2
Michael Pacleb <michael.pacleb@gmail.com>
2023-06-21 21:23:30 -0400

    updated users views and urls into viewsets and routers to explore its capability of reducing redundancy of same endpoints.

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail, UserList, UserDetail
+# from .views import PostList, PostDetail, UserList, UserDetail
+from .views import PostList, PostDetail, UserViewSet
+
+
+# urlpatterns = [
+#     path("", PostList.as_view(), name="post_list"),
+#     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+#     path("users/", UserList.as_view(), name="user_list"),
+#     path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
+# ]
+
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+
 
 urlpatterns = [
     path("", PostList.as_view(), name="post_list"),
     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("users/", UserList.as_view(), name="user_list"),
-    path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
-]
+] + router.urls
--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
+from rest_framework.permissions import IsAdminUser
 
 from django.contrib.auth import get_user_model
 from .models import Post
@@ -19,11 +20,17 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
 
 
-class UserList(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+# class UserList(generics.ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
